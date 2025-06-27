891d5c39fd72d44e169996f0c9b271caf2b6c07e
Leonardo <leonardo.so1104@gmail.com>
2025-06-27 10:15:47 -0300

    s3 implementations

---
+++ qrcode.generator/src/main/java/com/leonardo/qrcode/generator/dto/QrCodeGenerateRequest.java
@@ -0,0 +1,4 @@
+package com.leonardo.qrcode.generator.dto;
+
+public record QrCodeGenerateRequest(String text) {
+}
---
+++ qrcode.generator/src/main/java/com/leonardo/qrcode/generator/dto/QrCodeGenerateResponse.java
@@ -0,0 +1,4 @@
+package com.leonardo.qrcode.generator.dto;
+
+public record QrCodeGenerateResponse(String url) {
+}
